0a628d7c2bdd97543990d84104d49d9ad126f329
im genius <mail.usmanasif@gmail.com>
2017-11-02 19:34:42 +0500

    WIT

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { omniauth_callbacks: "omniauth_callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-
-  root  'dashboard#analytics'
+  root 'application#homepage'
+  
+  get  'dashboard/analytics'
 
   get 'dashboards/dashboard-social',
       to: 'dashboard#social',
