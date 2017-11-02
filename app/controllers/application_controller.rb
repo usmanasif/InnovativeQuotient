02c3bd28b452ec48e604e67fe39aa451ba8365c6
im genius <mail.usmanasif@gmail.com>
2017-11-02 19:53:03 +0500

    WIP

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 # :nodoc:
 class ApplicationController < ActionController::Base
   before_action :set_gettext_locale
-  #before_action :authenticate_user!, except: :homepage
+  before_action :authenticate_user!, except: [:homepage]
   before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
 
